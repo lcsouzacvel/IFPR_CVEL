730e17b64a9695d6e5f1e5019e16c1077b6117fd
lcsouzacvel <lcsouzacvel@gmail.com>
2023-03-28 17:08:25 -0300

    Atulizando projeto web

---
+++ web/atividades/api_clima.js
@@ -0,0 +1,19 @@
+// criando um objeto da API
+const api = {
+    key:"chave da api que ira ser utilizada"
+    base:"site da api"
+    lang: "idioma" 
+    units: "metricas"
+}
+
+
+const city = document.querySelector('.city');
+const date = document.querySelector('.date');
+const container_img = document.querySelector('.container-img');
+const container_temp = document.querySelector('.container-temp');
+const temp_number = document.querySelector('.container-temp div');
+const temp_unit = document.querySelector('.container-temp span');
+const weather_t = document.querySelector('.weather'); 
+const search_input = document.querySelector('.form-control');
+const search_button = document.querySelector('.btn');
+const low_high = document.querySelector('.low-high');
